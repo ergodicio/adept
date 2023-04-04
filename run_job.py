0e00ef01df861b17e53396efee96f0461ab0f8e0
archis <joglekara@users.noreply.github.com>
2023-04-03 17:53:51 -0700

    deploy test

--- run_job.py
+++ run_job.py
@@ -2,10 +2,10 @@
 #  research@ergodic.io
 
 import sys, os
-import run
+from utils.runner import start_run
 
 
 if __name__ == "__main__":
     run_id = sys.argv[1]
     run_type = sys.argv[2]
-    run.start_run(run_type, run_id)
+    start_run(run_type, run_id)
