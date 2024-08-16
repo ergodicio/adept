50093056fbfcf07234683c37e2e55c193310202f
archis <joglekara@users.noreply.github.com>
2024-08-16 13:47:50 -0700

    kappa calculation

--- adept/sbsbs1d/vectorfield.py
+++ adept/sbsbs1d/vectorfield.py
@@ -11,15 +11,23 @@ class ExponentialLeapfrog:
     def __init__(self, cfg: Dict) -> None:
         self.cfg = cfg
         self.dz = cfg["grid"]["dz"]
+        self.zax = cfg["grid"]["z"]
+        self.nc = cfg["units"]["derived"]["nc"]
 
     def calculate_noise(self, args: Dict[str, float]) -> float:
         return np.random.normal(0, np.sqrt(2 * args["nu_coll"] * self.dz))
 
-    def calc_kappa(self, args: Dict[str, float]) -> float:
-        return args["kappaIB"]
+    def calc_kappa(self, z, args: Dict[str, float]) -> float:
+        n_over_nc, TkeV = args["n_over_nc"](z), args["TkeV"](z)
+        vth = jnp.sqrt(TkeV)
+        ND = n_over_nc * (vth / omegap) ** 3.0
+        nuei_over_w0 = jnp.sqrt(n_over_nc) * self.nuei_const * self.logLambda(n, T) / ND
 
-    def calc_imfx0(self, args: Dict[str, float]) -> float:
-        return args["imfx0"]
+        return kappa_ib
+
+    def calc_imfx0(self, z, args: Dict[str, float]) -> float:
+        n, T = args["n"](z), args["T"](z)
+        return imfx0
 
     def __call__(self, t, y, args):
         Ji = y["Ji"]
