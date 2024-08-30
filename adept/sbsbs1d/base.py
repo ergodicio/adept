a9e2b6d8fee9b0fb8cc3ecb766eb38047051e9af
archis <joglekara@users.noreply.github.com>
2024-08-30 11:17:22 -0700

    units and profiles

--- adept/sbsbs1d/base.py
+++ adept/sbsbs1d/base.py
@@ -1,4 +1,7 @@
 from typing import Dict
+from astropy import constants as const
+from astropy.units import Quantity as _Q
+import numpy as np
 from adept import ADEPTModule
 
 
@@ -6,7 +9,39 @@ class BaseSteadyStateBackwardStimulatedBrilloiunScattering(ADEPTModule):
     def __init__(self, cfg) -> None:
         super().__init__(cfg)
 
-    def write_units(self) -> Dict:
+    def write_units(self, cfg) -> Dict:
+        """ """
+        m_e = const.m_e
+        e = const.e
+        c = const.c
+        eps_0 = const.eps0
+        mi = cfg["units"]["atomic mass number"] * const.m_p
+
+        lambda0 = _Q(cfg["units"]["laser_wavelength"]).to("nm")
+        T0 = _Q(cfg["units"]["reference temperature"]).to("keV")
+        vth0 = np.sqrt(2 * T0 / m_e).to("m/s")
+        w0 = (2 * np.pi * c / lambda0).to("Hz")
+        cs0 = np.sqrt(T0 / mi).to("m/s")
+        I0 = _Q(cfg["units"]["reference intensity"]).to("W/cm^2")
+        a0 = None
+
+        if cfg["units"]["reference density"] == "nc":
+            n0 = (2 * np.pi * c / cfg["units"]["laser_wavelength"]) ** 2.0 * m_e * eps_0 / e**2.0
+        else:
+            n0 = _Q(cfg["units"]["reference density"]).to("cm-3")
+
+        cfg["units"]["derived"] = {
+            "n0": n0,
+            "lambda0": lambda0,
+            "T0": T0,
+            "vth0": vth0,
+            "w0": w0,
+            "cs0": cs0,
+            "I0": I0,
+            "a0": a0,
+            "mi": mi,
+        }
+
         return {}
 
     def get_derived_quantities(self) -> Dict:
--- adept/sbsbs1d/vectorfield.py
+++ adept/sbsbs1d/vectorfield.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
-from jax import numpy as jnp, Array
+from jax import numpy as jnp, Array, random
+from jax.scipy.special import erfc
 import numpy as np
 
 from adept import get_envelope
@@ -10,24 +11,63 @@ class ExponentialLeapfrog:
 
     def __init__(self, cfg: Dict) -> None:
         self.cfg = cfg
+        self.PRNGKey = random.PRNGKey(0)
         self.dz = cfg["grid"]["dz"]
         self.zax = cfg["grid"]["z"]
         self.nc = cfg["units"]["derived"]["nc"]
+        self.ratio_M_m = cfg["units"]["derived"]["ratio_M_m"]
+
+        # self.logLambda = 5.0
+
+    def zprime(self, z: float) -> float:
+        return jnp.exp(-(z**2.0)) * erfc(-1j * z)
 
     def calculate_noise(self, args: Dict[str, float]) -> float:
-        return np.random.normal(0, np.sqrt(2 * args["nu_coll"] * self.dz))
+        return random.uniform(self.PRNGKey, (1,))
 
     def calc_kappa(self, z, args: Dict[str, float]) -> float:
-        n_over_nc, TkeV = args["n_over_nc"](z), args["TkeV"](z)
-        vth = jnp.sqrt(TkeV)
-        ND = n_over_nc * (vth / omegap) ** 3.0
-        nuei_over_w0 = jnp.sqrt(n_over_nc) * self.nuei_const * self.logLambda(n, T) / ND
+        n_over_nc, Te, Zeff = args["n_over_nc"](z), args["Te_keV"](z), args["Zeff"](z)
+        # vth = jnp.sqrt(TkeV)
+        # ND = n_over_nc * (vth / omegap) ** 3.0
+        # nuei_over_w0 = jnp.sqrt(n_over_nc) * self.nuei_const * self.logLambda / ND
 
+        plas_Lambda = self.nclam3 * Te**1.5 / (2 * jnp.pi) ** 3
+        nu_ei_bar = 1 / 12 / jnp.pi * jnp.sqrt(2 / jnp.pi) * Zeff * jnp.log(plas_Lambda) / plas_Lambda
+
+        nu_ei = density * nu_ei_bar  # Local electron-ion collision frequency/omega
+        nu_IB = density**2 * nu_ei_bar  # Inverse Bremstrahlung absorption rate/omega
+        # Electron-ion collision frequency at critical density/omega
+
+        kappa_ib = kz / omega * nu_IB
         return kappa_ib
 
     def calc_imfx0(self, z, args: Dict[str, float]) -> float:
-        n, T = args["n"](z), args["T"](z)
-        return imfx0
+        n_over_nc, Te, Ti, Zeff, flow, omega_beat = (
+            args["n_over_nc"](z),
+            args["Te_keV"](z),
+            args["Ti_keV"](z),
+            args["Zeff"](z),
+            args["flow"](z),
+            args["omega_beat"](z),
+        )
+
+        omega_p2_elec = n_over_nc  # omega_pe^2/omega^2
+        omega_p2_ion = Zeff / self.ratio_M_m * omega_p2_elec  # omega_pi^2/omega^2
+        omega_beat_plasframe = omega_beat + 2 * kz * flow  # Beat frequency in plasma frame/omega
+
+        v_th_e = jnp.sqrt(Te)  ## This is a placeholder
+        v_th_i = jnp.sqrt(Ti)  ## This is a placeholder
+
+        kz = self.kz_vac * jnp.sqrt(1 - n_over_nc)
+
+        # Electron susceptibility
+        chi_e = -omega_p2_elec / 8 / kz**2 / v_th_e**2 * self.zprime(omega_beat_plasframe / 2**1.5 / kz / v_th_e)
+
+        # Ion susceptibility
+        chi_i = -omega_p2_ion / 8 / self.kz**2 / v_th_i**2 * self.zprime(omega_beat_plasframe / 2**1.5 / kz / v_th_i)
+
+        F_chi = chi_e * (1.0 + chi_i) / (1.0 + chi_e + chi_i)
+        return jnp.imag(F_chi)
 
     def __call__(self, t, y, args):
         Ji = y["Ji"]
