ae6d35dbd14bca519d8f5f4edd66ccde87f4acf9
Jonathan Brodrick <jonathanbrodrick@gmail.com>
2024-12-08 17:40:20 +0000

    Implemented an inialisation of local form of f10 by taking an artific… (#91)
    * Implemented an inialisation of local form of f10 by taking an artificially large timestep while keeping f0 fixed. This is essentially equivalent to temporarily reducing the electron mass and thus the effect of electron inertia. I have tested this in an untracked adept notebook and seems to work well when using nuei_scaling. It is untested when including anisotropic ee collisions, but should work as well as the assumption that a tridiagonal representation of these is sufficient. If this turns out to not be the case we might have to use some iterative method or a dense matrix solve. Currently, this is hard-coded and should ideally be an option in the yaml file but I did not want to create an extra dependency in the yaml file and break any of your code, if you can advise how to add switches while maintaining backwards compatibility going forwards that would be much appreciated.
    
    Also implemented an option to boost isotropic electron-electron collisions to reduce nonlocal effects and restore to Maxwellian quicker. Note that at low Z electron inertia effects may still be prominent as the boost not impact this. Again no switch has been provided and the relevant lines have been commented out.
    
    Also moved anisotropic ee diag contributions in the self.ee if statement as there is no point calculating these if using nuei_scaling.
    
    Note vector_field is no longer in sync with oshun.py, whereas previously they were identical. We should consider removing oshun.py.
    
    Finally, a few minor comments/questions, some we have previously discussed.
    
    Happy to take on any feedback, thanks! :-) JPB
    
    * Resolved requested changes on requirements.txt and requirements-cpu.txt, used black linter through terminal, hope it worked!
    
    * Linted helpers.py with black --line-length 120 through terminal, other python files didn't seem to need linting
    
    ---------
    
    Co-authored-by: Jonathan Brodrick <jonathan.brodrick@york.ac.uk>

--- adept/vfp1d/vector_field.py
+++ adept/vfp1d/vector_field.py
@@ -98,6 +98,11 @@ class OSHUN1D:
 
         """
 
+        # Questions:
+        # Is there a better way to get ∆J/∆E? e.g. using jvp
+        # or at least reusing the two self.ei calls rather than running for a third time after?
+        # Also note that second order effects only come from v df0dx and f2 terms.
+
         # calculate j without any e field
         f10_after_coll = self.ei(Z=Z, ni=ni, f0=f0, f10=f10, dt=self.dt)
         j0 = self.calc_j(f10_after_coll)
@@ -105,7 +110,7 @@ class OSHUN1D:
         # get perturbation
         de = jnp.abs(e) * self.large_eps + self.eps
 
-        # calculate effect of dex
+        # calculate effect of dex (if we are doing it this way, would there be any harm using f0_after_dex too?)
         _, f10_after_dex = self.push_edfdv(f0, f10, de)
         f10_after_dex = self.ei(Z=Z, ni=ni, f0=f0, f10=f10_after_dex, dt=self.dt)
         jx_dx = self.calc_j(f10_after_dex)
