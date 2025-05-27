dbddbf828689d538fd3448a1ce8e54e9313c52b5
Jonathan Brodrick <jonathanbrodrick@gmail.com>
2025-05-27 11:22:40 +0100

    allowed unused import in select root files

---
+++ adept/lpse2d.py
@@ -0,0 +1,8 @@
+from ._lpse2d import (
+    BaseLPSE2D,
+    calc_threshold_intensity,
+    ArbitraryDriver,
+    UniformDriver,
+    GaussianDriver,
+    LorentzianDriver,
+)
---
+++ adept/vlasov1d.py
@@ -0,0 +1,1 @@
+from ._vlasov1d.modules import BaseVlasov1D
