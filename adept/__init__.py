11df9c8ba49ee991224583cb8e1885e06de9ee25
Jack Coughlin <jack.coughlin@simulation.science>
2025-12-09 15:30:00 -0800

    Implement AdroitModule
    Adds a new equinox module wrapper concept that optionally
    starts an mlflow run and passes the ID to pre_logging
    and post_logging methods.

--- adept/__init__.py
+++ adept/__init__.py
@@ -1,2 +1,3 @@
 from ._base_ import ADEPTModule, ergoExo  # noqa: I001
+from .adroit import AdroitModule
 from . import lpse2d, vlasov1d
---
+++ adept/adroit.py
@@ -0,0 +1,102 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+import mlflow
+import numpy as np
+
+
+class MLRunId(eqx.Module):
+    """
+    An mlflow run id encoded as a JAX array of bytes.
+    This class is needed to pass around the run id in JIT-compiled sections.
+    """
+
+    byte_array: jax.Array
+
+    def __init__(self, id: str):
+        self.byte_array = jnp.frombuffer(id.encode("UTF-8"), dtype=jnp.int8)
+
+    def __str__(self):
+        return bytes(np.array(self.byte_array)).decode("UTF-8")
+
+    @staticmethod
+    def example():
+        return MLRunId("0" * 32)
+
+
+class AdroitModule(eqx.Module, ABC):
+    with_mlflow: bool
+
+    @staticmethod
+    def create_mlflow_run_in_callback(dummy_arg):
+        def create_mlflow_run(dummy_arg):
+            with mlflow.start_run() as run:
+                return MLRunId(run.info.run_id)
+
+        return jax.experimental.io_callback(
+            create_mlflow_run, MLRunId.example(), dummy_arg, sharding=None, ordered=False
+        )
+
+    def call_pre_logging_in_callback(self, *args, mlflow_run_id, **kwargs):
+        def pre_logging_call(*args, mlflow_run_id, **kwargs):
+            self.pre_logging(*args, mlflow_run_id=str(mlflow_run_id), **kwargs)
+
+        jax.debug.callback(pre_logging_call, *args, mlflow_run_id=mlflow_run_id, **kwargs)
+
+    def call_post_logging_in_callback(self, result, *args, mlflow_run_id, **kwargs):
+        def post_logging_call(result, *args, mlflow_run_id, **kwargs):
+            self.post_logging(result, *args, mlflow_run_id=str(mlflow_run_id), **kwargs)
+
+        jax.debug.callback(post_logging_call, result, *args, mlflow_run_id=mlflow_run_id, **kwargs)
+
+    def __call__(self, *args, mlflow_batch_num=None, **kwargs):
+        """
+        Params:
+            - `*args`: The list of positional arguments to pass to `call`
+            - `mlflow_batch_num`: A dummy argument to ensure that multiple mlflow runs
+                                  are opened when we are `vmap`ping over `__call__`. Must
+                                  be specified by the caller.
+                                  Outside of a vmap context, mlflow_batch_num=0 is an appropriate choice.
+                                  When vmapping, you should use `mlflow_batch_num=jnp.arange(batch_size)`.
+            - `**kwargs`: The dict of keyword args to pass to `call`
+        """
+        if self.with_mlflow:
+            if mlflow_batch_num is None:
+                raise ValueError("mlflow_batch_num must be specified")
+            run_id = AdroitModule.create_mlflow_run_in_callback(mlflow_batch_num)
+        else:
+            run_id = None
+
+        if self.with_mlflow:
+            self.call_pre_logging_in_callback(*args, mlflow_run_id=run_id, **kwargs)
+
+        result = self.call(*args, mlflow_run_id=run_id, **kwargs)
+
+        if self.with_mlflow:
+            self.call_post_logging_in_callback(result, *args, mlflow_run_id=run_id, **kwargs)
+
+        return result
+
+    @abstractmethod
+    def call(self, *args, **kwargs):
+        """
+        The computation to wrap.
+        """
+        pass
+
+    @abstractmethod
+    def pre_logging(self, *args, mlflow_run_id=None, **kwargs):
+        """
+        Any mlflow logging to be performed before the computation, such as logging params.
+        """
+        pass
+
+    @abstractmethod
+    def post_logging(self, result, *args, mlflow_run_id=None, **kwargs):
+        """
+        Any mlflow logging to be performed after the computation.
+        """
+        pass
