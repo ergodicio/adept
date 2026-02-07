b4a4d4370f1e89aaf2eab969b9c84f33096f19f1
archis <joglekara@users.noreply.github.com>
2026-02-07 15:08:19 -0800

    linter

--- adept/_spectrax1d/__init__.py
+++ adept/_spectrax1d/__init__.py
@@ -5,6 +5,6 @@ Wrapper for the spectrax library to solve Vlasov-Maxwell equations using
 Hermite-Fourier decomposition with ADEPT's MLflow logging infrastructure.
 """
 
-from adept._spectrax1d.modules import BaseSpectrax1D, EPW1D
+from adept._spectrax1d.modules import EPW1D, BaseSpectrax1D
 
-__all__ = ["BaseSpectrax1D", "EPW1D"]
+__all__ = ["EPW1D", "BaseSpectrax1D"]
