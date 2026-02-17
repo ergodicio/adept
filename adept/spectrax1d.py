1d6cddc6baedd1f36774a207d733958a254e8a61
archis <joglekara@users.noreply.github.com>
2026-02-17 15:24:22 -0800

    more cleanup

--- adept/spectrax1d.py
+++ adept/spectrax1d.py
@@ -2,4 +2,3 @@
 
 from adept._spectrax1d.modules import EPW1D as EPW1D
 from adept._spectrax1d.modules import BaseSpectrax1D as BaseSpectrax1D
-from adept._spectrax1d.modules import HermiteSRS1D as HermiteSRS1D
